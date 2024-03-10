9096293895e2bb828cd1cf52bf2078e3428992ec
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-10 22:17:35 +0200

    fixed edit button

--- scripts/transac.js
+++ scripts/transac.js
@@ -35,7 +35,7 @@ const deleteData = (index) => {
 
 const editData = (index) => {
   let transacs = getTransac();
-  document.getElementById("submit").style.display = "none";
+  document.getElementById("transac-btn").style.display = "none";
   document.getElementById("update").style.display = "block";
   transacAmount.value = transacs[index].transacAmount;
   transacCurrency.value = transacs[index].transacCurrency;
@@ -47,15 +47,16 @@ const editData = (index) => {
     if (validateTransac(transacs[index])) {
       updateTransac(transacs[index]);
       updateDataView();
-      document.getElementById("submit").style.display = "block";
+      document.getElementById("transac-btn").style.display = "block";
       document.getElementById("update").style.display = "none";
     } else {
       updateDataView();
-      document.getElementById("submit").style.display = "block";
+      document.getElementById("transac-btn").style.display = "block";
       document.getElementById("update").style.display = "none";
     }
   };
 };
+
 
 const updateDataView = () => {
   const transacs = getTransac();
@@ -76,8 +77,6 @@ const validateTransac = (transac) => {
     ? (alert("Currency is required"), false)
     : transac.transacType === ""
     ? (alert("transacType is required"), false)
-    : !transac.transacType === ""
-    ? (alert("Invalid transacType address"), false)
     : true;
 };
 
@@ -90,8 +89,8 @@ const generateTransacIntoHtml = (transacList) => {
         <li>${element.transacCurrency}</li>
         <li>${element.transacType}</li>
         <li>
-          <button onclick="deleteData(${index})" class="btn btn-danger">Delete</button>
-          <button onclick="editData(${index})" class="btn btn-warning">Edit</button>
+          <button onclick="deleteData(${index})" class="del-edit-btn">Delete</button>
+          <button onclick="editData(${index})" class="del-edit-btn">Edit</button>
         </li>
       </ul>
     `;
