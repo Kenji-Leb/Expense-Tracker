347e04d0e6ab226a4dc941443a7d647a88c1f78d
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-10 21:33:36 +0200

    first fetch worked

--- scripts/fetching.js
+++ scripts/fetching.js
@@ -1,13 +1,49 @@
-fetch('https://crowded-cyan-wildebeest.cyclic.app/students/available', {
-  method: 'GET'
+fetch('https://rich-erin-angler-hem.cyclic.app/students/available', {
+    method: 'GET'
 })
-  .then(response => response.text())
-  .then(data => alert(data));
+.then(response => response.json())
+.then(data => {
+    const codes = data.map(currency => currency.code);
+    const currencySelects = document.querySelectorAll('#transacCurrency, #currency');
 
-fetch('https://crowded-cyan-wildebeest.cyclic.app/students/convert', {
-  method: 'POST',
-  body: new FormData(document.querySelector('#test-fetch'))
-})
-  .then(response => response.text())
-  .then(data => alert(data)); 
+    currencySelects.forEach(select => {
+        codes.forEach(code => {
+            const option = document.createElement('option');
+            option.value = code;
+            option.textContent = code;
+            select.appendChild(option);
+        });
+    });
+});
+
+// [
+//     {
+//         "name": "United States Dollar",
+//         "symbol": "$",
+//         "code": "USD"
+//     },
+//     {
+//         "name": "Euro",
+//         "symbol": "€",
+//         "code": "EUR"
+//     },
+//     {
+//         "name": "United Arab Emirates Dirham",
+//         "symbol": "dh",
+//         "code": "AED"
+//     },
+//     {
+//         "name": "Lebanese Pound",
+//         "symbol": "L.L.",
+//         "code": "LBP"
+//     }
+// ]
+
+// fetch('https://crowded-cyan-wildebeest.cyclic.app/students/convert', {
+//   method: 'POST',
+//   body: new FormData(document.querySelector('#test-fetch'))
+// })
+//   .then(response => response.text())
+//   .then(data => alert(data)); 
   
+
